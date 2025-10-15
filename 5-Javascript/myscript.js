05a6b02ba554c87b7189c1ce807ad114b38b460c
SathwikGoud12 <sathwikgoud851@gmail.com>
2025-10-15 20:43:54 +0530

    first commit

---
+++ 5-Javascript/myscript.js
@@ -0,0 +1,6 @@
+window.document.write("Welcome to JavaScript");
+let a=10;
+let b=20;
+let sum=a+b;
+window.document.write("<br/>Sum is: "+sum);
+console.log("Sum is: "+sum);
---
+++ Node.js/NJS2.Filewrite.js
@@ -0,0 +1,2 @@
+const myfs=require("fs");
+myfs.writeFileSync("data.txt","Good Morning");
